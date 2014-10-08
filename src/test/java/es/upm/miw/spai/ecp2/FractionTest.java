6515d6b07f57301097b21136fcd4aece86617bbc
kateMoscoso <katiemscs@gmail.com>
2014-10-08 07:35:06 +0200

    TestMayorFraction #9

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -8,30 +8,37 @@ import org.junit.Test;
 import es.upm.miw.spai.ecp2.Fraction;
 
 public class FractionTest {
- private Fraction pt; 
-  
-  
-    @Before 
-    public void before() { 
-        pt = new Fraction(5, 2); 
-    }
+	private Fraction pt; 
+	private Fraction pt2; 
+
+	@Before 
+	public void before() { 
+		pt = new Fraction(5, 2);
+
+	}
 
 
-    @Test 
-    public void testNumeradorInt() { 
-        assertEquals(5, pt.getNumerator()); 
-    }
+	@Test 
+	public void testNumeradorInt() { 
+		assertEquals(5, pt.getNumerator()); 
+	}
 
-    @Test 
-    public void testDenominadorInt() { 
-        assertEquals(2, pt.getDenominator()); 
-    }
+	@Test 
+	public void testDenominadorInt() { 
+		assertEquals(2, pt.getDenominator()); 
+	}
 
- 
-    @Test 
-    public void testDecimal() { 
-        assertEquals(2.5, pt.decimal(), 10e-5); 
-    } 
-    
-  
+
+	@Test 
+	public void testDecimal() { 
+		assertEquals(2.5, pt.decimal(), 10e-5); 
+	} 
+	@Test
+	public void testisMayor(){
+		pt2= pt.isMayor(pt, new Fraction());
+		assertEquals(5, pt2.getNumerator()); 
+		assertEquals(2, pt2.getDenominator()); 
+
+	}
+
 }
